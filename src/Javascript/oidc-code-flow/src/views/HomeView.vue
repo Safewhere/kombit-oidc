dc0a8bc16fe60b333fad6d4ca2977ac48669c4f4
pmc@globeteam.com <pmc@globeteam.com>
2025-03-31 11:07:20 +0700

    Bug 1833: [FK-11][JavaScript client] Add support for more parameters

--- src/Javascript/oidc-code-flow/src/views/HomeView.vue
+++ src/Javascript/oidc-code-flow/src/views/HomeView.vue
@@ -18,32 +18,20 @@
             <input type="number" id="max-age" v-model="maxAge" />
           </div>
           <div v-if="!user">
-            <button @click="login(false, false)">Login</button>
+            <button @click="login(false, false)">Sign in</button>
           </div>
           <template v-if="user">
             <h4>Welcome, {{ user.profile.name }}</h4>
-            <button @click="logout">Logout</button>
-            <button @click="login(true, false)">Force authn</button>
-            <button @click="login(false, true)">Passive login</button>
+            <button @click="login(false, false)">Re-Authenticate</button>
+            <button @click="login(true, false)">Force Authentication</button>
+            <button @click="login(false, true)">Passive Authentication</button>
+            <button @click="logout">Sign out</button>
           </template>
         </div>
       </div>
     </header>
 
     <div v-if="user">
-      <div v-if="accessToken">
-        <h2>Access Token</h2>
-        <p class="token">{{ accessToken }}</p>
-        <div class="decoded" v-if="accessTokenHeader">
-          <h3>Header</h3>
-          <p>{{ accessTokenHeader }}</p>
-        </div>
-        <div class="decoded" v-if="accessTokenPayload">
-          <h3>Payload</h3>
-          <p>{{ accessTokenPayload }}</p>
-        </div>
-      </div>
-
       <div v-if="idToken">
         <h2>ID Token</h2>
         <p class="token">{{ idToken }}</p>
@@ -54,6 +42,19 @@
         <div class="decoded" v-if="idTokenPayload">
           <h3>Payload</h3>
           <p class="token">{{ idTokenPayload }}</p>
+        </div>
+      </div>
+
+      <div v-if="accessToken">
+        <h2>Access Token</h2>
+        <p class="token">{{ accessToken }}</p>
+        <div class="decoded" v-if="accessTokenHeader">
+          <h3>Header</h3>
+          <p>{{ accessTokenHeader }}</p>
+        </div>
+        <div class="decoded" v-if="accessTokenPayload">
+          <h3>Payload</h3>
+          <p>{{ accessTokenPayload }}</p>
         </div>
       </div>
     </div>
